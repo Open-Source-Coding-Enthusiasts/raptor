7053bb575e1372a92ae2ad41beddb000a330e2e7
oskar-duda <o.duda@poczta.onet.pl>
2024-02-24 11:43:13 +0100

    feat(embeddings): initial code structure

---
+++ tests/test_create_embeddings.py
@@ -0,0 +1,8 @@
+from src import embedding
+
+
+def test_create_embeddings():
+    texts = ['car', 'pet', 'carpet']
+    embeddings = embedding.create_embeddings(texts)
+
+    assert isinstance(embeddings, dict)
